db1969bd7b1ae8ece70ef68ded661fab216518b0
Samuel R <samuel_-_rojas@hotmail.com>
2019-07-04 10:36:21 -0500

    WIP: template

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -9,17 +9,10 @@ import { AbstractControl, FormGroup, FormBuilder, Validators } from '@angular/fo
 export class LoginComponent implements OnInit {
 
   validateForm: FormGroup;
-  isCollapsed = false;
-  triggerTemplate = null;
-  @ViewChild('trigger', {static: true}) customTrigger: TemplateRef<void>;
 
   constructor(private fb: FormBuilder) {
   }
 
-  /** custom trigger can be TemplateRef **/
-  changeTrigger(): void {
-    this.triggerTemplate = this.customTrigger;
-  }
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       userName: [ null, [ Validators.required ] ],
@@ -29,6 +22,7 @@ export class LoginComponent implements OnInit {
   }
 
   submitForm(): void {
+
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[ i ].markAsDirty();
       this.validateForm.controls[ i ].updateValueAndValidity();
