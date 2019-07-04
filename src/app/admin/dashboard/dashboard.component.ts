46661e44ebab399ddcd4de2016fe28b324dbc674
Samuel R <samuel_-_rojas@hotmail.com>
2019-07-03 19:48:10 -0500

    WIP: Make template

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,6 +7,43 @@ import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   isCollapsed: boolean = false;
+  mapOfExpandData: { [key: string]: boolean } = {};
+  listOfData = [
+    {
+      id: 1,
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+    },
+    {
+      id: 2,
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+    },
+    {
+      id: 3,
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+    }
+  ];
+
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
 
   ngOnInit(): void {
   }
