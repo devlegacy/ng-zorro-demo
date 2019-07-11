b7913634600b41256089b5c07077339bf549fbd1
Samuel R <samuel_-_rojas@hotmail.com>
2019-07-11 10:47:10 -0500

    Update app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { AuthenticationService } from './shared/services/authentication.service'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Ng Zorro Demo';
+  title:String = 'App - Project';
+  isCollapsed:boolean = false;
   constructor(public _authService:AuthenticationService){}
 }
