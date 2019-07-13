0846359e85a9bc5d7107a34688cf86eb24204367
Samuel R <samuel_-_rojas@hotmail.com>
2019-07-13 10:30:26 -0500

    WIP: Issues and logout/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, TemplateRef } from '@angular/core';
 import { AuthenticationService } from './shared/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ import { AuthenticationService } from './shared/services/authentication.service'
 export class AppComponent {
   title:String = 'App - Project';
   isCollapsed:boolean = false;
-  constructor(public _authService:AuthenticationService){}
+  constructor(public _authService:AuthenticationService, public _router:Router){}
+
+  logout() {
+    this._authService.logOut();
+    this._router.navigate(['/login']);
+  }
 }
